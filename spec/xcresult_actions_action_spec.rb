cd450bc4d7a5de59cf322cc90db0ce8ee2261bb4
yutailang0119 <muta.yutaro@gmail.com>
2020-06-15 01:23:29 +0900

    Remove xcresult_actions files

--- spec/xcresult_actions_action_spec.rb
+++
@@ -1,9 +0,0 @@
-describe Fastlane::Actions::XcresultActionsAction do
-  describe '#run' do
-    it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with("The xcresult_actions plugin is working!")
-
-      Fastlane::Actions::XcresultActionsAction.run(nil)
-    end
-  end
-end
